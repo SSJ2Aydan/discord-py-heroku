d0815720b260bd093a01d264da4b3692326a6508
SSJ2Aydan <78390792+SSJ2Aydan@users.noreply.github.com>
2022-02-26 11:48:28 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 @client.command()
 async def play(ctx, url_: str):
+  await bot.change_presence(activity=discord.Game(name="coconut mall"))
   voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='coconut mall theme')
   await voiceChannel.connect()
   voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
